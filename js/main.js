213e36f78d1dad3021b53217e86702dc95639759
Ramielson <ramielsonedgar@gmail.com>
2023-06-06 19:46:32 +0300

    test-6

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
   const checkbox = document.querySelector('.check-box__input')
-const bannerImg  =document.querySelector('.banner-container__img')
+  const bannerImg = document.querySelector('.banner-container__img')
+  
+ 
+ 
 
-checkbox.addEventListener('change',()=>{
-  if(checkbox.checked) {
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      bannerImg.classList.add('show')
+      const store =  localStorage.setItem("banner", "off")
+     
+    } else {
+      bannerImg.classList.remove('show')
+      localStorage.clear();
+    }
+
+  
+  })
+
+
+  if(localStorage.getItem("banner") !== null ) {
     bannerImg.classList.add('show')
+    checkbox.checked = true
   } else {
     bannerImg.classList.remove('show')
+    localStorage.clear();
   }
-})
+
 })
